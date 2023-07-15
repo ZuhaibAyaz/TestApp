b78dd5293bff8bf4e2bab227d793d82cb652e328
Zuhaib Ayaz <zuhaibayaz7@gmail.com>
2023-07-15 17:36:19 +0530

    refactor

--- test/src/components/Body.jsx
+++ test/src/components/Body.jsx
@@ -1,26 +1,42 @@
-import React from 'react'
-import Slider from './Slider'
-import SubNav from './SubNav'
-import Cards from './Cards'
+import React from "react";
+import Slider from "./Slider";
+import SubNav from "./SubNav";
+import Cards from "./Cards";
 
 function Body() {
   return (
     <div>
       <SubNav></SubNav>
-  <Slider></Slider>
+      <Slider></Slider>
 
-
-
-
-<Cards></Cards>
-
-
-
-
-
-
+      <div class="container text-center ">
+        <div class="row">
+          <div class="col-lg-4 col-md-6 col-sm">
+            <Cards/>
+          </div>
+          <div class="col-lg-4 col-md-6 col-sm">
+            
+            <Cards/>
+          </div>
+          <div class="col-lg-4 col-md-6 col-sm">
+            
+            <Cards/>
+          </div>
+          <div class="col-lg-4 col-md-6 col-sm">
+            <Cards/>
+          </div>
+          <div class="col-lg-4 col-md-6 col-sm">
+            
+            <Cards/>
+          </div>
+          <div class="col-lg-4 col-md-6 col-sm">
+            
+            <Cards/>
+          </div>
+        </div>
+      </div>
     </div>
-  )
+  );
 }
 
-export default Body
+export default Body;
