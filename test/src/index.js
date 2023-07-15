f671368e34328ec82a1b8df37eecaeb479059e09
Zuhaib Ayaz <zuhaibayaz7@gmail.com>
2023-07-15 15:08:36 +0530

    refactor

--- test/src/index.js
+++ test/src/index.js
@@ -1,12 +1,24 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
+import {BrowserRouter, Routes, Route} from 'react-router-dom'
+import Nav from './components/Nav';
+import Master from './Master';
+
 
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    hello
+  
+   <BrowserRouter>
+   <Nav/>
+   <Routes>
+
+   <Route path="/" element={<Master/>} />
+
+   </Routes>
+   </BrowserRouter>
   </React.StrictMode>
 );
 
