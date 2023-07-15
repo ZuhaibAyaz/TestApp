ef0ba98e0c5acb8239404b15d472ebb21c11a7fd
Zuhaib Ayaz <zuhaibayaz7@gmail.com>
2023-07-15 17:06:23 +0530

    feat

---
+++ test/src/components/Cards.jsx
@@ -0,0 +1,21 @@
+import React from 'react'
+
+function Cards() {
+  return (
+    <div className="card-container">
+  <div className="card" >
+  <img src="..." class="card-img-top" alt="..."/>
+  <div className="card-body">
+    <h5 className="card-title">Card title</h5>
+    <h6> Sub Title</h6>
+    <p className="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+    
+  </div>
+</div>
+
+
+    </div>
+  )
+}
+
+export default Cards
