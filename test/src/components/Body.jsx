80f7fb6e9861b21a45b3c3e4ed142d49995f8efd
Zuhaib Ayaz <zuhaibayaz7@gmail.com>
2023-07-15 17:06:11 +0530

    feat-cards

--- test/src/components/Body.jsx
+++ test/src/components/Body.jsx
@@ -1,12 +1,23 @@
 import React from 'react'
 import Slider from './Slider'
 import SubNav from './SubNav'
+import Cards from './Cards'
 
 function Body() {
   return (
     <div>
       <SubNav></SubNav>
   <Slider></Slider>
+
+
+
+
+<Cards></Cards>
+
+
+
+
+
 
     </div>
   )
